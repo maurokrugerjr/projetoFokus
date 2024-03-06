6c796d3803a06b892fd15de61df5ead32c1b9563
maurokrugerjr <maurinho_k@hotmail.com>
2024-03-06 14:43:29 -0300

    cria logica pra adicionar class e selecionar um botão

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,9 @@ const textarea = document.querySelector('.app__form-textarea')
 const storedData = localStorage.getItem('tarefas');
 const tarefas = storedData ? JSON.parse(storedData) : [];
 const ulTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
+
+let tarefaSelecionada = null 
 
 function limparFormulario() {
     textarea.value = ''
@@ -49,6 +52,21 @@ function criarElementoTarefa(tarefa){
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+        if(tarefaSelecionada == tarefa){
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null    
+            return
+        }
+        tarefaSelecionada = tarefa
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        li.classList.add('app__section-task-list-item-active')
+    }
 
     return li
 }
